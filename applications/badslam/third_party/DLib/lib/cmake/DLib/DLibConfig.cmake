96f768390aacf07c2512ba4fa2e7574a44dbe44d
Jure Kovac <jure.kovac@gmail.com>
2022-01-11 11:01:41 +0100

    added DLib prebuild from: https://github.com/dorian3d/DLib.git to thirt_party
    added opengv prebuild from:  https://github.com/laurentkneip/opengv.git
    to third_party

---
+++ applications/badslam/third_party/DLib/lib/cmake/DLib/DLibConfig.cmake
@@ -0,0 +1,16 @@
+
+set(DLIB_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../../DLib")
+# message("DLib root dir: ${DLIB_ROOT_DIR}")
+
+FIND_LIBRARY(DLib_LIBRARY DLib
+    PATHS "${DLIB_ROOT_DIR}/lib"
+)
+FIND_PATH(DLib_INCLUDE_DIR DLibConfig.cmake
+    PATHS "${DLIB_ROOT_DIR}/include/DLib" 
+)
+LIST(APPEND DLib_INCLUDE_DIR
+  ${DLib_INCLUDE_DIR}/../ ${DLib_INCLUDE_DIR}/../DUtils
+  ${DLib_INCLUDE_DIR}/../DUtilsCV ${DLib_INCLUDE_DIR}/../DVision)
+SET(DLib_LIBRARIES ${DLib_LIBRARY})
+SET(DLib_LIBS ${DLib_LIBRARY})
+SET(DLib_INCLUDE_DIRS ${DLib_INCLUDE_DIR})
