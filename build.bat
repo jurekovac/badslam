c4f0147f9f32a81c8f87999ac2373a3216f4ff37
DESKTOP-R6C2DTL\fiztok <ifekonja@gmail.com>
2022-06-28 15:05:50 +0200

    vcpkg

--- build.bat
+++ build.bat
@@ -4,13 +4,31 @@ cd %~dp0
 set MYPATH=%CD%
 echo.
 
+rem F:/Scanner/G2/Dev/App/current/CS8 ...CSbadslam?
+rem F:/Scanner/G2/Dev/3rdparty/vcpkg/vcpkg-export/
+rem VCPKG-EXPORT=F:\Scanner\G2\Dev\3rdparty\vcpkg\vcpkg-export-28.06.2022
+rem INSTALL_DIR= F:\Scanner\G2\Dev\App\current\install 
+rem              F:/Scanner/G2/Dev/App/current/CSBadSlam/
+
 set USE_VS2022=0
+
+set VCPKG_EXPORT=%MYPATH%\..\..\..\3rdparty\vcpkg\vcpkg-export
+set INSTALL_DIR=%MYPATH%\..\install
+
+set QTOPENSSL=C:\Qt\Tools\OpenSSL\Win_x64\bin
+
+if not exist %VCPKG_EXPORT% set VCPKG_EXPORT=%MYPATH%\..\vcpkg-export 
+
+echo USING VCPKG_EXPORT %VCPKG_EXPORT%
+echo USING INSTALL_DIR %INSTALL_DIR%
 
 rem https://github.com/facebookresearch/pytorch3d/issues/1227
 rem https://github.com/NVIDIA/cub/tree/1.16.X
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
 
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\NVIDIA Corporation\GPU Computing Toolkit\CUDA\v11.6" /v 64BitInstalled
+if %ERRORLEVEL% NEQ 0 goto CUDA_11_6_VAR_INSTALLED
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\NVIDIA Corporation\GPU Computing Toolkit\CUDA\v11.6" /v InstallDir
 if %ERRORLEVEL% EQU 0 goto CUDA_11_6_VAR_INSTALLED
 reg add   "HKEY_LOCAL_MACHINE\SOFTWARE\NVIDIA Corporation\GPU Computing Toolkit\CUDA\v11.6" /v InstallDir /t REG_SZ /d "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.6" /f
@@ -50,11 +68,10 @@ set QTBIN=%QTDIR%\msvc2015_64\bin
 if  exist %QTDIR%\msvc2019_64\bin set QTBIN=%QTDIR%\msvc2019_64\bin
 echo %QTBIN%
 
-set QTOPENSSL=%QTBIN%\Tools\OpenSSL\Win_x64\bin
 
-if exist \vcpkg-export\installed\x64-windows\include\qt5 set QTDIR=\vcpkg-export\installed\x64-windows\include\qt5
-if exist \vcpkg-export\installed\x64-windows\bin set QTBIN=\vcpkg-export\installed\x64-windows\bin
-if exist \vcpkg-export\installed\x64-windows\tools\openssl set QTOPENSSL=\vcpkg-export\installed\x64-windows\tools\openssl
+if exist %VCPKG_EXPORT%\installed\x64-windows\include\qt5 set QTDIR=%VCPKG_EXPORT%\installed\x64-windows\include\qt5
+if exist %VCPKG_EXPORT%\installed\x64-windows\bin set QTBIN=%VCPKG_EXPORT%\installed\x64-windows\bin
+if exist %VCPKG_EXPORT%\installed\x64-windows\tools\openssl set QTOPENSSL=%VCPKG_EXPORT%\installed\x64-windows\tools\openssl
 
 set QTROOT=%QTDIR%\..
 set DEVENV=start /b /wait ""  devenv.com
@@ -70,7 +87,7 @@ rem if exist build %RMDIR% build
 if not exist build mkdir build
 cd build
 
-set TOOLCHAIN="/vcpkg-export/scripts/buildsystems/vcpkg.cmake"
+set TOOLCHAIN="%VCPKG_EXPORT%/scripts/buildsystems/vcpkg.cmake"
 set COMPILE_DEBUG=0
 set COMPILE_KEYW="Release"
 
@@ -94,9 +111,8 @@ echo ================= COMPILE BADSLAM %COMPILE_KEYW% ======================
 echo .
 echo .
 
-if %USE_VS2022% == 1 cmake -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN% -A x64 -T cuda=11.6 -DBADSLAM_DIR=/CSBadSlam -DBADSLAM_BUILD_DIR=/CSBadSlam/build -DCMAKE_CUDA_ARCHITECTURES="75;86"  ..
-else                 cmake -G "Visual Studio 16 2019" -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN% -A x64 -T cuda=11.6 -DBADSLAM_DIR=/CSBadSlam -DBADSLAM_BUILD_DIR=/CSBadSlam/build -DCMAKE_CUDA_ARCHITECTURES="75;86"  ..
-
+if %USE_VS2022% == 1 cmake -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=%VCPKG-EXPORT%\scripts\buildsystems\vcpkg.cmake -A x64 -T cuda=11.6 -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DBADSLAM_DIR=%MYPATH% -DBADSLAM_BUILD_DIR=%MYPATH%/build -DCMAKE_CUDA_ARCHITECTURES="75;86"  ..
+else                 cmake -G "Visual Studio 16 2019" -DCMAKE_TOOLCHAIN_FILE=%VCPKG-EXPORT%\scripts\buildsystems\vcpkg.cmake -A x64 -T cuda=11.6 -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DBADSLAM_DIR=%MYPATH% -DBADSLAM_BUILD_DIR=%MYPATH%/build -DCMAKE_CUDA_ARCHITECTURES="75;86"  ..
 
 if %COMPILE_DEBUG% == 0 (
 	cmake --build . --target install --config Release
